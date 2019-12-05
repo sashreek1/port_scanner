133f583a0b5974e750eec7ee308e592044d1ec47
sashreek1 <45600974+sashreek1@users.noreply.github.com>
2019-12-05 11:33:39 +0530

    Update port_test.py

--- port_test.py
+++ port_test.py
@@ -1,6 +1,6 @@
 import socket
 
-for port in range(1, 65535):
+for port in range(1, 65536):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(0.25)
     result = sock.connect_ex(("192.168.1.1", port))
