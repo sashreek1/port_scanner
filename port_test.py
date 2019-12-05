3c60c04fbc28d1a201e604dd960639c1459e81e7
sashreek1 <45600974+sashreek1@users.noreply.github.com>
2019-12-05 11:33:21 +0530

    Add files via upload

---
+++ port_test.py
@@ -0,0 +1,9 @@
+import socket
+
+for port in range(1, 65535):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(0.25)
+    result = sock.connect_ex(("192.168.1.1", port))
+    if 0 == result:
+        print("Port: {} Open".format(port))
+    sock.close()
